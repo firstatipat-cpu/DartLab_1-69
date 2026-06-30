860808ffb834d398f9f7eaa2e0a506f71bd1068f
unknown <firstatipat@gmail.com>
2026-06-30 09:13:06 +0700

    complete

--- lab_1.dart
+++ lab_1.dart
@@ -3,18 +3,20 @@ void main() {
   print(sub.length) ;
   print('first sub : ${sub[0]}\nlast sub : ${sub.last}');
   sub.add('PE');
-  print(sub);
+  print("all sub : ${sub}");
   var studentScores = {
     'Data Structure' : 50 , 'Operation System' : 40 , 'Java' : 75
   } ;
-  print(studentScores['Data Structure']);
+  print('Data Structure Score : ${studentScores['Data Structure']}');
   studentScores['Python'] = 65 ;
-  print(studentScores);
+  print('all Scores : ${studentScores}');
+  print('sub that contain "a" in it :');
   for (var i in studentScores.entries) {
     if (i.key.contains('a')) {
       print('${i.key} : ${i.value}');
-    } 
+    }
   }
+  print('sub with a Score of more than or equal to 50 :');
   for (var i in studentScores.entries) {
     if (i.value >= 50) {
       print('${i.key} : ${i.value}');
