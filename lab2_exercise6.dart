2a15c45579b04b5773355b618b8006aadae46af6
ATP <firstatipat@gmail.com>
2026-07-04 21:25:49 +0700

    ex 6
    Removed the addition of the base area in the cylinder calculation.

--- lab2_exercise6.dart
+++ lab2_exercise6.dart
@@ -5,7 +5,7 @@ void executeOperation(double h , double w , double Function(double,double) opera
 }
 double tri(double h , double w) => (0.5*h*w);
 double squ(double h , double w) => (h*w);
-double cyl(double h , double w) => ((pi*pow((w/2),2)*h) +2*(pi*pow((w/2),2)));
+double cyl(double h , double w) => (pi*pow((w/2),2)*h);
 
 void main(){
   executeOperation(3,4,tri);
